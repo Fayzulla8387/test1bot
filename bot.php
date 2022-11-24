2635c3e82a5d9a8486fded64dd83b66a9bbf915e
Fayzulla8387 <xojayevfayzulla7@gmail.com>
2022-11-24 14:28:26 +0500

    Initial commit

---
+++ bot.php
@@ -0,0 +1,10 @@
+<?php
+require_once "telegram.php";
+$telegram=new Telegram("5869126547:AAHVuiF1-pcPiTyldLE68NmHhRfGnzewIAM");
+$chat_id=$telegram->ChatID();
+$text=$telegram->Text();
+$telegram->sendMessage([
+'chat_id'=>$chat_id,
+'text'=>$text
+]);
+
