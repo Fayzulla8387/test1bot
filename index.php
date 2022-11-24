80075d9074474555eb680c16cc51dfd8bc0aed72
Fayzulla8387 <xojayevfayzulla7@gmail.com>
2022-11-24 20:22:11 +0500

    zzz

--- index.php
+++ index.php
@@ -3,10 +3,23 @@ require_once "telegram.php";
 $telegram=new Telegram("5869126547:AAHVuiF1-pcPiTyldLE68NmHhRfGnzewIAM");
 $chat_id=$telegram->ChatID();
 $text=$telegram->Text();
+if($text=="/start"){
+$telegram->sendMessage([
+'chat_id'=>$chat_id,
+'text'=>"Welcome to my bot"
+]);
+
+}
+else{
 $telegram->sendMessage([
 'chat_id'=>$chat_id,
 'text'=>$text
 ]);
+
+}
+
+
+
 
 
 
