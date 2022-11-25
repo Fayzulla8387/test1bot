e845895079951d6a61b3593a1a7db7bdeb3b536e
Fayzulla8387 <xojayevfayzulla7@gmail.com>
2022-11-25 09:16:23 +0500

    zzz

--- index.php
+++ index.php
@@ -4,8 +4,18 @@ $telegram=new Telegram("5869126547:AAHVuiF1-pcPiTyldLE68NmHhRfGnzewIAM");
 $chat_id=$telegram->ChatID();
 $text=$telegram->Text();
 if($text=="/start"){
+    $option = array(
+        //First row
+        array($telegram->buildKeyboardButton("Nagap"), $telegram->buildKeyboardButton("Button 2")),
+        //Second row
+        array($telegram->buildKeyboardButton("Button 3"), $telegram->buildKeyboardButton("Button 4"), $telegram->buildKeyboardButton("Button 5")),
+        //Third row
+        array($telegram->buildKeyboardButton("Button 6")) );
+    $keyb = $telegram->buildKeyBoard($option, $onetime=false);
+
 $telegram->sendMessage([
 'chat_id'=>$chat_id,
+"reply_markup" => $keyb,
 'text'=>"Salom mening ismim Fayzulla"
 ]);
 }
@@ -14,7 +24,6 @@ $telegram->sendMessage([
 'chat_id'=>$chat_id,
 'text'=>$text
 ]);
-
 }
 
 
